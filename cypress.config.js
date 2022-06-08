75ee98c456f3b0c0f33b5fa22f024a5ab037f815
Paul de Witt <paul@dewittconsulting.nl>
2022-06-08 21:50:37 +0200

    updated config

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'https://www.meetup.com/',
     excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
   },
